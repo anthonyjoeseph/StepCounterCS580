4a2c2a386bdb72a49d1be296338af1b07dbc4a09
Anthony Gabriele <agabrie5@binghamton.edu>
2016-04-19 15:37:52 -0400

    First commit, blank screen, basic activity

---
+++ app/src/main/java/com/example/cs580s/stepsensor/StepActivity.java
@@ -0,0 +1,13 @@
+package com.example.cs580s.stepsensor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step);
+    }
+}
